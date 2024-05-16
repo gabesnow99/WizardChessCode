dfcb474a968bea87b12a191fc9fbe0d2df2864f3
gabesnow99 <86386869+gabesnow99@users.noreply.github.com>
2024-05-15 23:34:08 -0600

    Added limit switch functionality - working

--- Integrated Test/Electromagnet_Integrated_Test/Electromagnet_Integrated_Test.ino
+++ Integrated Test/Electromagnet_Integrated_Test/Electromagnet_Integrated_Test.ino
@@ -2,6 +2,7 @@
 
 // Global Integers
 #define BUTTON_THRESHOLD 500
+#define DIGITAL_THRESHOLD .5
 #define SPR 2038 // steps per revolution
 #define SPEED 14 
 #define MAP1 250
@@ -18,6 +19,10 @@
 #define LED_pin 13
 #define EM_pin 3
 
+// Limit Switch
+#define limit_south 2
+#define limit_west 12
+
 // Variables
 int outer_pot_val = 0;
 int inner_pot_val = 0;
@@ -32,6 +37,11 @@ float last_timestamp = 0;
 
 // Functions
 bool isButtonPushed();
+void pennyGoHome();
+void stepNorth(int steps);
+void stepSouth(int steps);
+void stepEast(int steps);
+void stepWest(int steps);
 // void handleInterrupt(); // BUG: LED status seems to be accurate but often button is unreliable
 
 
@@ -42,28 +52,32 @@ void setup() {
   pinMode(inner_pot_pin, INPUT);
   pinMode(button_pin, INPUT_PULLUP);
   pinMode(EM_pin, OUTPUT);
+  pinMode(limit_south, INPUT_PULLUP);
+  pinMode(limit_west, INPUT_PULLUP);
   outer_axis.setSpeed(SPEED);
   inner_axis.setSpeed(SPEED);
+
+  pennyGoHome();
 
   // attachInterrupt(digitalPinToInterrupt(button_pin), handleInterrupt, FALLING); // BUG: LED status seems to be accurate but often button is unreliable
 }
 
 void loop() {
+
   // Cycle Speed Debugger
   cycle_speed = millis() - time;
   time = millis();
-  Serial.print("cycle_speed: ");
-  Serial.print(cycle_speed);
-
+  // Serial.print("cycle_speed: ");
+  // Serial.print(cycle_speed);
 
   // // joystick variable debugger
   outer_pot_val = analogRead(outer_pot_pin);
   inner_pot_val = analogRead(inner_pot_pin);
   button_val = analogRead(button_pin);
-  Serial.print(" toggled: ");
-  Serial.print(toggled);
-  Serial.print(" button_val: ");
-  Serial.println(button_val);
+  // Serial.print(" toggled: ");
+  // Serial.print(toggled);
+  // Serial.print(" button_val: ");
+  // Serial.println(button_val);
   // Serial.print(" outer_pot_val: ");
   // Serial.print(outer_pot_val);
   // Serial.print(" inner_pot_val: ");
@@ -95,41 +109,50 @@ void loop() {
     digitalWrite(EM_pin, LOW);
   }
 
-  // Control Axies
-  if (outer_pot_val < MAP1 /*&& !toggled*/){
+  Serial.print("limit south: ");
+  Serial.print(digitalRead(limit_south));
+  Serial.print(" limit west: ");
+  Serial.print(digitalRead(limit_west));
+  Serial.print("\n");
+
+  // Control Axes
+  if (digitalRead(limit_south) || digitalRead(limit_west)) {
+    return;
+  }
+  if (outer_pot_val < MAP1 /*&& !toggled*/) {
     outer_axis.step(SPR / 50);
   }
-  if (outer_pot_val > MAP2 /*&& !toggled*/){
+  if (outer_pot_val > MAP2 /*&& !toggled*/) {
     outer_axis.step(-SPR / 50);
   }
-  if (inner_pot_val < MAP3 /*&& !toggled*/){
+  if (inner_pot_val < MAP3 /*&& !toggled*/) {
     inner_axis.step(SPR / 50);
   }
-  if (inner_pot_val > MAP4 /*&& !toggled*/){
+  if (inner_pot_val > MAP4 /*&& !toggled*/) {
     inner_axis.step(-SPR/ 50);
   }
-  // if (outer_pot_val < MAP1 && toggled){
-  //   outer_axis.step(SPR / 200);
-  //   delay(200);
-  // }
-  // if (outer_pot_val > MAP2 && toggled){
-  //   outer_axis.step(-SPR / 200);
-  //   delay(200);
-  // }
-  // if (inner_pot_val < MAP3 && toggled){
-  //   inner_axis.step(SPR / 200);
-  //   delay(200);
-  // }
-  // if (inner_pot_val > MAP4 && toggled){
-  //   inner_axis.step(-SPR/ 200);
-  //   delay(200);
-  // }
+//   if (outer_pot_val < MAP1 && toggled){
+//     outer_axis.step(SPR / 200);
+//     delay(200);
+//   }
+//   if (outer_pot_val > MAP2 && toggled){
+//     outer_axis.step(-SPR / 200);
+//     delay(200);
+//   }
+//   if (inner_pot_val < MAP3 && toggled){
+//     inner_axis.step(SPR / 200);
+//     delay(200);
+//   }
+//   if (inner_pot_val > MAP4 && toggled){
+//     inner_axis.step(-SPR/ 200);
+//     delay(200);
+//   }
 }
 
 
 // Function Definitions
-bool isButtonPushed(){
-  if (analogRead(button_pin) <= BUTTON_THRESHOLD){
+bool isButtonPushed() {
+  if (analogRead(button_pin) <= BUTTON_THRESHOLD) {
     if (toggled == false)
     {
       toggled = true;
@@ -143,6 +166,31 @@ bool isButtonPushed(){
   } else {
     return false;
   }
+}
+
+void pennyGoHome() {
+  while (!digitalRead(limit_south)) {
+    stepSouth(10);
+  }
+  while (!digitalRead(limit_west)) {
+    stepWest(10);
+  }
+}
+
+void stepNorth(int steps = SPR/50) {
+  inner_axis.step(steps);
+}
+
+void stepSouth(int steps = SPR/50) {
+  inner_axis.step(-steps);
+}
+
+void stepEast(int steps = SPR/50) {
+  outer_axis.step(steps);
+}
+
+void stepWest(int steps = SPR/50) {
+  outer_axis.step(-steps);
 }
 
 // void handleInterrupt(){ // BUG: LED status seems to be accurate but often button is unreliable 
