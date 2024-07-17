46f3b1764795ae5c269e66003eb421718244dbfc
gabesnow99 <86386869+gabesnow99@users.noreply.github.com>
2024-07-16 22:12:03 -0600

    Update collision_detection.py

--- Isolated Tests/collision_detection_test/collision_detection.py
+++ Isolated Tests/collision_detection_test/collision_detection.py
@@ -2,13 +2,32 @@
 #-------------------------- It takes the coordinates the piece needed to move, the destinitation, and is_kill as arguments
 #-------------------------- It returns the "G-Code" for where the motors need to move and when the electromagnet needs to activate/deactivate
 
-def gcode(piece: str, destination: str, is_kill: bool) -> str:
+class Waypoint:
+
+    def __init__(self, x='A', y=1, is_final_point=False):
+        self.x = x
+        self.y = y
+        self.is_final_point = is_final_point
+
+    # CONVERTS WAYPOINT DATA TO STRING FOR SERIAL COMMUNICATION
+    def get_as_string(self):
+        to_return = self.x
+        to_return += str(self.y)
+        # to_return += str(int(self.is_final_point))
+        return to_return
+
+
+def gcode(game_board, piece: str, destination: str, is_kill: bool) -> str:
     print(f'gcode({piece}, {destination}, {is_kill}) executed!')
-    code: str = ''
+    waypoints: str = ''
     if is_kill:
         handle_the_kill()
     find_direct_route()
-    return code
+    # TODO: ENDPONTS INDICATE LENGTH OF GCODE
+    return waypoints
+
+def chart_path(game_board, ):
+    pass
 
 def handle_the_kill(): #TODO FINISH THIS FUNCTION
     pass
@@ -22,3 +41,10 @@ def find_next_collision(): #TODO FINDS THE NEXT COLLISON. RETURNS A USEFULL VARI
 
 def create_gcode() -> str: #TODO DETERMINE WHEN THIS SHOULD BE CALLED. RETURNS THE GCODE TO BE READ BY THE ARDUINO
     pass
+
+
+if __name__=="main":
+    a = Waypoint("B", 3)
+    b = Waypoint("C", 4)
+    print(a.get_as_string())
+    print(b.get_as_string())
